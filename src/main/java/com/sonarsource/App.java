b2981779a85a65c8978d69ffef133e8858db8356
Aurélie Boiteux <aurelie.boiteux@sonarsource.com>
2020-03-04 13:09:07 +0000

    App.java edited online with Bitbucket

--- src/main/java/com/sonarsource/App.java
+++ src/main/java/com/sonarsource/App.java
@@ -11,7 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        //TODO a todo
+        //TODO a todo blabla
     }
 
     public void newMethod(){
