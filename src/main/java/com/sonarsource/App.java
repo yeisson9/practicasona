61641c5aa3e4e70678e696cc7c8c4835e19b7714
admin <aurelie.boiteux@sonarsource.com>
2020-03-05 09:58:21 +0100

    Adding issues

--- src/main/java/com/sonarsource/App.java
+++ src/main/java/com/sonarsource/App.java
@@ -11,5 +11,19 @@ public class App
         System.out.println( "Hello World!" );
         //TODO a todo
     }
+    private static final int MAX_PRICE = 10;
+    public void setPriceGitChecks(int price) {
+        assert price >= 0 && price <= MAX_PRICE;
+        // Set the price
+    }
+
+
+
+
+    public void aMethod(){
+        int i=0;
+        while (i<10){
+            i++;
+        }}
 
 }
