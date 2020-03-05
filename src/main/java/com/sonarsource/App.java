2c9344e7099a8c401e5905b48c78489d2ae20bf8
admin <aurelie.boiteux@sonarsource.com>
2020-03-05 10:01:55 +0100

    Adding issues

--- src/main/java/com/sonarsource/App.java
+++ src/main/java/com/sonarsource/App.java
@@ -26,4 +26,24 @@ public class App
             i++;
         }}
 
+
+    class A {
+        void foo(boolean a, Object b) throws MyException {
+            Object o = bar(a, b);
+            if (a) {
+                o.toString(); // HERE
+            }
+        }
+        private Object bar(boolean b, Object o) throws MyException {
+            if (b) {
+                return null;
+            }
+            if (o == null)  {
+                throw new MyException();
+            }
+            return o;
+        }
+    }
+    class MyException extends Exception { }
+
 }
