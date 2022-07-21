858fe083713386cf64916272d30007abcf4969a3
yeisson9 <109741783+yeisson9@users.noreply.github.com>
2022-07-21 09:40:01 -0500

    Update App.java

--- src/main/java/com/sonarsource/App.java
+++ src/main/java/com/sonarsource/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
          System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
     }
 
     public void newMethodGitchecks(){
